6ac6d8113d99c111898c37ae931dcd41617d4137
gabriel-vitebo <vitebo@outlook.com>
2024-10-28 14:30:45 -0300

    create product use case

--- src/domain/entities/Product.ts
+++ src/domain/entities/Product.ts
@@ -1,11 +1,19 @@
+import {randomUUID} from "node:crypto";
+
 export class Product {
+    id: string
     title: string
     price: number
+    initialAmount: number
+    currentQuantity: number
     description: string
 
-    constructor(title: string, price: number, description: string) {
+    constructor(title: string, price: number, description: string, initialAmount: number, currentQuantity?: number, id?: string) {
         this.title = title
         this.price = price
         this.description = description
+        this.initialAmount = initialAmount
+        this.currentQuantity = currentQuantity ?? initialAmount
+        this.id = id ?? randomUUID()
     }
 }
--- src/domain/entities/User.ts
+++ src/domain/entities/User.ts
@@ -1,7 +1,11 @@
+import {randomUUID} from "node:crypto";
+
 export class User {
+    id: string
     name: string
 
-    constructor(name: string) {
+    constructor(name: string, id?: string ) {
+        this.id = id ?? randomUUID()
         this.name = name
     }
 }
