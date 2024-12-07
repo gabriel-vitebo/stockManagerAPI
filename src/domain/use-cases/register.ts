5eefc1cf699c23f3fb9aa94491b4ad22ba5d7149
gabriel-vitebo <vitebo@outlook.com>
2024-12-07 12:28:39 -0300

    remove entities

--- src/domain/use-cases/register.ts
+++ src/domain/use-cases/register.ts
@@ -1,6 +1,5 @@
 import { hash } from 'bcryptjs'
 import { UsersRepository } from '@/domain/repositories/users-repository'
-import { User } from '../entities/User'
 
 interface RegisterUseCaseRequest {
   name: string
@@ -9,7 +8,7 @@ interface RegisterUseCaseRequest {
 }
 
 export class RegisterUseCase {
-  constructor(private usersRepository: UsersRepository) {}
+  constructor(private usersRepository: UsersRepository) { }
 
   async execute({ name, email, password }: RegisterUseCaseRequest) {
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
@@ -20,15 +19,9 @@ export class RegisterUseCase {
 
     const passwordHash = await hash(password, 10)
 
-    const user = User.create({
+    await this.usersRepository.create({
       name,
       email,
-      password: passwordHash,
-    })
-
-    await this.usersRepository.create({
-      name: user.name,
-      email: user.email,
       password_hash: passwordHash,
     })
   }
