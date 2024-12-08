7a4b963f5f502f975008505e473e9068a4875dd1
Gabriel Vitebo <110203065+gabriel-vitebo@users.noreply.github.com>
2024-12-08 15:31:15 -0300

    add EditProductUseCase (#1)

--- src/domain/repositories/product-repository.ts
+++ src/domain/repositories/product-repository.ts
@@ -2,5 +2,16 @@ import { Prisma, Product } from '@prisma/client'
 
 export interface ProductRepository {
   create(data: Prisma.ProductUncheckedCreateInput): Promise<void>
+  getById(id: string): Promise<{
+    id: string
+    title: string
+    price: string
+    description: string
+    initialAmount: number
+    currentQuantity: number
+    createdAt: Date
+    userId: string
+  } | null>
   fetchAll(userId: string): Promise<Product[]>
+  update(data: Prisma.ProductUncheckedCreateInput): Promise<Product>
 }
--- src/http/controllers/products/routes.ts
+++ src/http/controllers/products/routes.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from 'fastify'
 import { createProduct } from './create-product'
 import { fetchAllProducts } from './fetchAllProducts'
+import { updateProduct } from './update'
 
 export async function productRoutes(app: FastifyInstance) {
   app.post('/products/new', createProduct)
+  app.put('/products/update/:id', updateProduct)
   app.get('/products/:userId', fetchAllProducts)
 }
