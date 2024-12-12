b65dc8a57b78d28a5005e1c53940638a71c5f8d1
gabriel-vitebo <vitebo@outlook.com>
2024-12-12 01:14:51 -0300

    update use case

--- src/http/controllers/products/update.ts
+++ src/http/controllers/products/update.ts
@@ -1,43 +1,25 @@
-import { FastifyRequest, FastifyReply } from 'fastify'
-import { z } from 'zod'
-import { PrismaProductRepository } from '@/domain/repositories/prisma/prisma-product-repository'
-import { UpdateProductUseCase } from '@/domain/use-cases/updateProduct'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { prisma } from '../../../lib/prisma' // Supondo que você esteja usando o Prisma ou outra ORM
+import { Prisma } from '@prisma/client'
 
 export async function updateProduct(
   request: FastifyRequest,
   reply: FastifyReply,
 ) {
-  const updateProductBodySchema = z.object({
-    title: z.string(),
-    description: z.string(),
-    price: z.string(),
-    initialAmount: z.number(),
-  })
-
-  const updateProductParamsSchema = z.object({
-    id: z.string(),
-  })
-
-  const { title, description, price, initialAmount } =
-    updateProductBodySchema.parse(request.body)
-
-  const { id } = updateProductParamsSchema.parse(request.params)
+  const { id } = request.params as { id: string }
+  const updatedData = request.body as Prisma.ProductUpdateInput
 
   try {
-    const productRepository = new PrismaProductRepository()
-    const updateUseCase = new UpdateProductUseCase(productRepository)
-
-    await updateUseCase.execute({
-      id,
-      title,
-      description,
-      price,
-      initialAmount,
+    const updatedProduct = await prisma.product.update({
+      where: { id },
+      data: updatedData,
     })
-  } catch (error) {
-    console.error(error)
-    return reply.status(409).send()
-  }
 
-  return reply.status(201).send()
+    return reply.status(200).send({ success: true, product: updatedProduct })
+  } catch (error) {
+    console.error('Failed to update product:', error)
+    return reply
+      .status(500)
+      .send({ success: false, error: 'Failed to update product' })
+  }
 }
